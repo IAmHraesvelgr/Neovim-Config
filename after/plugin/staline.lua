0d6fe6a6bdeb05e04934cdc5ccea3a213a9fe7c5
Hræsvelgr <alexis.kahne@free.fr>
2024-01-26 09:25:43 +0100

    nvim config

--- after/plugin/staline.lua
+++ after/plugin/staline.lua
@@ -1,11 +1,42 @@
-require('staline').setup()
-require('stabline').setup {
-  style = "bar",
-  stab_left = "┃",
-  stab_right = "",
+require('staline').setup {
 
-  font_active = "bold",
-  exclude_fts = { 'NvimTree', 'dashboard', 'lir' },
-  stab_start = "",
-  stab_end = ""
+	sections = {
+		left = {
+			' ', 'right_sep_double', '-mode', 'left_sep_double', ' ',
+			'right_sep', '-file_name', 'left_sep', ' ',
+			'right_sep_double', '-branch', 'left_sep_double', ' ',
+		},
+		mid  = {'lsp'},
+		right= {
+			'right_sep', '-cool_symbol', 'left_sep', ' ',
+			'right_sep', '- ', '-lsp_name', '- ', 'left_sep',
+			'right_sep_double', '-line_column', 'left_sep_double', ' ',
+		}
+	},
+
+	defaults={
+		fg = "#986fec",
+		cool_symbol = "  ",
+		left_separator = "",
+		right_separator = "",
+		-- line_column = "%l:%c [%L]",
+		true_colors = true,
+		line_column = "[%l:%c] 並%p%% "
+		-- font_active = "bold"
+	},
+	mode_colors = {
+		n  = "#181a23",
+		i  = "#181a23",
+		ic = "#181a23",
+		c  = "#181a23",
+		v  = "#181a23"       -- etc
+	}
+}
+
+require('stabline').setup {
+    style = "slant",
+    bg = "#986fec",
+    fg = "black",
+    stab_right = "",
+    font_active = "bold"
 }
