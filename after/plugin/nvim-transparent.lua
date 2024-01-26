2b7299c7bd2a71a3502214506a6dc6f277755393
Hræsvelgr <alexis.kahne@free.fr>
2024-01-26 11:52:59 +0100

    nvim config

--- after/plugin/nvim-transparent.lua
+++ after/plugin/nvim-transparent.lua
@@ -9,3 +9,5 @@ require("transparent").setup({ -- Optional, you don't have to run setup.
   extra_groups = {}, -- table: additional groups that should be cleared
   exclude_groups = {}, -- table: groups you don't want to clear
 })
+
+vim.cmd [[TransparentEnable]]
