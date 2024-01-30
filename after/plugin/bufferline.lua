6138821e70f3994f65dfaf15bb4214a388b77aee
Hræsvelgr <alexis.kahne@free.fr>
2024-01-30 16:35:04 +0100

    nvim config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,3 +1,7 @@
-require("bufferline").setup()
+require("bufferline").setup({
+  options = {
+    separator_style = "thick"
+  }
+})
 vim.keymap.set("n", "<leader>bn", vim.cmd.BufferLineCycleNext)
 vim.keymap.set("n", "<leader>bp", vim.cmd.BufferLineCyclePrev)
