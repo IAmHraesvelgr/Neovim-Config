6205aefab634d34c98ac073e2f1997a7e552e222
Hræsvelgr <alexis.kahne@free.fr>
2024-02-01 11:25:33 +0100

    nvim config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,7 @@
 require('bufferline').setup()
 
 vim.keymap.set("n", "<leader>bc", vim.cmd.BufferLinePickClose)
+vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
 vim.keymap.set("n", "<leader>bn", vim.cmd.BufferLineCycleNext)
 vim.keymap.set("n", "<leader>bp", vim.cmd.BufferLineCyclePrev)
 vim.keymap.set("n", "<leader>ba", vim.cmd.BufferLineCloseOthers)
