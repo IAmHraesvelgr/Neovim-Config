647d27a147b9609740fde15686ea0f63ba61a9d9
Hræsvelgr <alexis.kahne@free.fr>
2024-02-08 09:58:24 +0100

    nvim config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,11 @@
 vim.cmd.colorscheme("catppuccin")
+
+require("catppuccin").setup({
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+    }
+})
