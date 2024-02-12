344153f62844bb44c4f4436942fa0847966e8baa
Hræsvelgr <alexis.kahne@free.fr>
2024-02-12 10:55:21 +0100

    nvim config

--- lua/hraesvelgr/set.lua
+++ lua/hraesvelgr/set.lua
@@ -2,12 +2,14 @@
 
 vim.opt.nu = true
 
+vim.opt.relativenumber = true
+
 vim.loader.enable()
 
 vim.opt.laststatus = 2
 vim.opt.showtabline = 2
 
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
