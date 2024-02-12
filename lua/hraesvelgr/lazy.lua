fd36742da7ce440b569ca58756b83cebfc5446be
Hræsvelgr <alexis.kahne@free.fr>
2024-02-12 16:00:52 +0100

    nvim config

--- lua/hraesvelgr/lazy.lua
+++ lua/hraesvelgr/lazy.lua
@@ -73,7 +73,20 @@ require("lazy").setup({
   },
   "Pocco81/auto-save.nvim",
   'nvim-lualine/lualine.nvim',
-  "windwp/nvim-autopairs",
+  {
+    "windwp/nvim-autopairs",
+    opts = {
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+
+    -- setup cmp for autopairs
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
   'akinsho/bufferline.nvim',
   "folke/zen-mode.nvim",
   {
